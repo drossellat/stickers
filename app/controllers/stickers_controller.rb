9977a4558272d7717d7c85ad70fe34249267f699
David Rossellat <david.rossellat@gmail.com>
2013-11-26 17:41:00 +0700

    adding strong param and validation

--- app/controllers/stickers_controller.rb
+++ app/controllers/stickers_controller.rb
@@ -13,9 +13,18 @@ class StickersController < ApplicationController
     render json: @stickers
   end
   def show
-  	@sticker = Sticker.find(params[:id])
-  	render json: @sticker
+    if params[:id].match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i)
+      begin
+    	 @sticker = Sticker.find(params[:id])
+    	 render json: @sticker
+      rescue ActiveRecord::RecordNotFound
+        render :json => "record not found"
+      end
+    else
+      render :json => "invalid key"
+    end
   end
+
   def create
   	##
   	@sticker = Sticker.new(params[:sticker].permit(:title, :description, :URL, :size, :free))
--- app/models/sticker.rb
+++ app/models/sticker.rb
@@ -1,6 +1,6 @@
 class Sticker < ActiveRecord::Base
-	scope :is_free, -> { where(free: true) }
-	scope :is_not_free, -> { where(free: false) }
+	validates :title, :URL, presence: true
+	validates :size, presence: true, inclusion: { in: %w(small big), message: "%{value} is not a valid size" }
 	scope :is_big, -> { where(size: "big") }
 	scope :is_small, -> { where(size: "small") }
 end
