6757c524145a02248a427bf785e624e661c32d6e
David Rossellat <david.rossellat@gmail.com>
2013-11-30 13:46:05 +0700

    adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Sticker.create(title: 'LINE sticker 1', description: 'small sticker', URL: 'https://s3-ap-southeast-1.amazonaws.com/goodies-squar-store-testing/testSticker_01_small.png', size: 'small')
+Sticker.create(title: 'LINE sticker 1', description: 'big sticker', URL: 'https://s3-ap-southeast-1.amazonaws.com/goodies-squar-store-testing/testSticker_01_big.png', size: 'big')
+Sticker.create(title: 'LINE sticker 2', description: 'small sticker', URL: 'https://s3-ap-southeast-1.amazonaws.com/goodies-squar-store-testing/testSticker_02_small.png', size: 'small')
+Sticker.create(title: 'LINE sticker 2', description: 'big sticker', URL: 'https://s3-ap-southeast-1.amazonaws.com/goodies-squar-store-testing/testSticker_02_big.png', size: 'big')
+Sticker.create(title: 'LINE sticker 3', description: 'small sticker', URL: 'https://s3-ap-southeast-1.amazonaws.com/goodies-squar-store-testing/testSticker_03_small.png', size: 'small')
+Sticker.create(title: 'LINE sticker 3', description: 'big sticker', URL: 'https://s3-ap-southeast-1.amazonaws.com/goodies-squar-store-testing/testSticker_03_big.png', size: 'big')
+Sticker.create(title: 'LINE sticker 4', description: 'small sticker', URL: 'https://s3-ap-southeast-1.amazonaws.com/goodies-squar-store-testing/testSticker_04_small.png', size: 'small')
+Sticker.create(title: 'LINE sticker 4', description: 'big sticker', URL: 'https://s3-ap-southeast-1.amazonaws.com/goodies-squar-store-testing/testSticker_04_big.png', size: 'big')
